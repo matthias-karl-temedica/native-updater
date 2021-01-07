1dfeed1e459d3ed4e7f44c0f0c971636d35879c5
Matthias Karl <matthias.karl@temedica.com>
2021-01-07 09:46:46 +0100

    feat: automatically find config files

--- src/application/read-file.ts
+++ src/application/read-file.ts
@@ -1,8 +1,25 @@
+import { Dirent } from "fs";
+
 const fs = require("fs");
 const readline = require("readline");
+const { resolve } = require("path");
+const { readdir } = require("fs").promises;
+
+async function getFiles(dir: string, search: string) {
+  const dirents: Dirent[] = await readdir(dir, { withFileTypes: true });
+  const files: Array<string> = await Promise.all(
+    dirents.map((dirent) => {
+      const res = resolve(dir, dirent.name);
+      return dirent.isDirectory() ? getFiles(res, search) : res;
+    })
+  );
+
+  return files.flat().find((file) => file?.includes(search));
+}
 
 async function checkVersion(inputPath: string, key: string) {
-  const fileStream = fs.createReadStream(inputPath);
+  const file = await getFiles(".", inputPath);
+  const fileStream = fs.createReadStream(file);
 
   const rl = readline.createInterface({
     input: fileStream,
@@ -18,7 +35,7 @@ async function checkVersion(inputPath: string, key: string) {
         .trim()
         .replace("=", "")
         .split(" ")
-        .filter((char) => !!char)[1]
+        .filter((char: string) => !!char)[1]
         .replaceAll('"', "")
         .replaceAll(";", "");
 
