cb2d304a84d36dbf1454e4007b9001f7554f4828
Matthias Karl <matthias.karl@temedica.com>
2021-01-07 10:16:42 +0100

    refactor: dont search for files in read-file

--- src/application/read-file.ts
+++ src/application/read-file.ts
@@ -1,25 +1,8 @@
-import { Dirent } from "fs";
-
 const fs = require("fs");
 const readline = require("readline");
-const { resolve } = require("path");
-const { readdir } = require("fs").promises;
-
-async function getFiles(dir: string, search: string) {
-  const dirents: Dirent[] = await readdir(dir, { withFileTypes: true });
-  const files: Array<string> = await Promise.all(
-    dirents.map((dirent) => {
-      const res = resolve(dir, dirent.name);
-      return dirent.isDirectory() ? getFiles(res, search) : res;
-    })
-  );
-
-  return files.flat().find((file) => file?.includes(search));
-}
 
 async function checkVersion(inputPath: string, key: string) {
-  const file = await getFiles(".", inputPath);
-  const fileStream = fs.createReadStream(file);
+  const fileStream = fs.createReadStream(inputPath);
 
   const rl = readline.createInterface({
     input: fileStream,
